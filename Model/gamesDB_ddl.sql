48e05d6944ae16c11c38549e72ffd09e2bac2017
samuelceron <samceronm@gmail.com>
2020-12-08 19:36:25 -0600

    Updating DER, ddl and script ofr QuickDB

--- Model/gamesDB_ddl.sql
+++ Model/gamesDB_ddl.sql
@@ -1,160 +1,160 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/ALbpIU
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Games" (
-    "appid" int   NOT NULL,
-    "name" varchar(250)   NOT NULL,
-    "release_date" date   NOT NULL,
-    "language" int   NOT NULL,
-    "developer" int   NOT NULL,
-    "publisher" int   NOT NULL,
-    "required_age" int   NOT NULL,
-    "achievements" int   NOT NULL,
-    "positive_ratings" double   NOT NULL,
-    "negative_ratings" double   NOT NULL,
-    "average_playtime" double   NOT NULL,
-    "median_playtime" double   NOT NULL,
-    "owners" int   NOT NULL,
-    "price" float   NOT NULL,
-    "website" varchar(400)   NOT NULL,
-    "support_website" varchar(400)   NOT NULL,
-    "support_email" varchar(400)   NOT NULL,
-    CONSTRAINT "pk_Games" PRIMARY KEY (
-        "appid"
+CREATE TABLE Games (
+    app_id int   NOT NULL,
+    name varchar(250)   NOT NULL,
+    release_date date   NOT NULL,
+    language int   NOT NULL,
+    developer int   NOT NULL,
+    publisher int   NOT NULL,
+    required_age int   NOT NULL,
+    achievements int   NOT NULL,
+    positive_ratings decimal   NOT NULL,
+    negative_ratings decimal   NOT NULL,
+    average_playtime decimal   NOT NULL,
+    median_playtime decimal   NOT NULL,
+    owners int   NOT NULL,
+    price decimal   NOT NULL,
+    website varchar(400)   NOT NULL,
+    support_website varchar(400)   NOT NULL,
+    support_email varchar(400)   NOT NULL,
+    CONSTRAINT pk_Games PRIMARY KEY (
+        app_id
      )
 );
 
-CREATE TABLE "Developers" (
-    "developerid" int   NOT NULL,
-    "name" varchar(300)   NOT NULL,
-    CONSTRAINT "pk_Developers" PRIMARY KEY (
-        "developerid"
+CREATE TABLE Developers (
+    developer_id int   NOT NULL,
+    name varchar(300)   NOT NULL,
+    CONSTRAINT pk_Developers PRIMARY KEY (
+        developer_id
      )
 );
 
-CREATE TABLE "Publishers" (
-    "publisherid" int   NOT NULL,
-    "name" varchar(300)   NOT NULL,
-    CONSTRAINT "pk_Publishers" PRIMARY KEY (
-        "publisherid"
+CREATE TABLE Publishers (
+    publisher_id int   NOT NULL,
+    name varchar(300)   NOT NULL,
+    CONSTRAINT pk_Publishers PRIMARY KEY (
+        publisher_id
      )
 );
 
-CREATE TABLE "Language" (
-    "languageid" int   NOT NULL,
-    "language" varchar(20)   NOT NULL,
-    CONSTRAINT "pk_Language" PRIMARY KEY (
-        "languageid"
+CREATE TABLE Language (
+    language_id int   NOT NULL,
+    language varchar(20)   NOT NULL,
+    CONSTRAINT pk_Language PRIMARY KEY (
+        language_id
      )
 );
 
-CREATE TABLE "Platforms" (
-    "platformid" int   NOT NULL,
-    "name" varchar   NOT NULL,
-    CONSTRAINT "pk_Platforms" PRIMARY KEY (
-        "platformid"
+CREATE TABLE Platforms (
+    platform_id int   NOT NULL,
+    name varchar   NOT NULL,
+    CONSTRAINT pk_Platforms PRIMARY KEY (
+        platform_id
      )
 );
 
-CREATE TABLE "Categories" (
-    "categorieid" int   NOT NULL,
-    "name" varchar(150)   NOT NULL,
-    CONSTRAINT "pk_Categories" PRIMARY KEY (
-        "categorieid"
+CREATE TABLE Categories (
+    category_id int   NOT NULL,
+    name varchar(150)   NOT NULL,
+    CONSTRAINT pk_Categories PRIMARY KEY (
+        category_id
      )
 );
 
-CREATE TABLE "Genres" (
-    "genreid" int   NOT NULL,
-    "name" varchar(150)   NOT NULL,
-    CONSTRAINT "pk_Genres" PRIMARY KEY (
-        "genreid"
+CREATE TABLE Genres (
+    genre_id int   NOT NULL,
+    name varchar(150)   NOT NULL,
+    CONSTRAINT pk_Genres PRIMARY KEY (
+        genre_id
      )
 );
 
-CREATE TABLE "Tags" (
-    "tagid" int   NOT NULL,
-    "name" varchar(150)   NOT NULL,
-    CONSTRAINT "pk_Tags" PRIMARY KEY (
-        "tagid"
+CREATE TABLE Tags (
+    tag_id int   NOT NULL,
+    name varchar(150)   NOT NULL,
+    CONSTRAINT pk_Tags PRIMARY KEY (
+        tag_id
      )
 );
 
-CREATE TABLE "Owners" (
-    "ownersid" int   NOT NULL,
-    "range" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_Owners" PRIMARY KEY (
-        "ownersid"
+CREATE TABLE Owners (
+    owners_id int   NOT NULL,
+    range varchar(30)   NOT NULL,
+    CONSTRAINT pk_Owners PRIMARY KEY (
+        owners_id
      )
 );
 
-CREATE TABLE "Game_Platform" (
-    "appid" int   NOT NULL,
-    "platformid" int   NOT NULL,
-    CONSTRAINT "pk_Game_Platform" PRIMARY KEY (
-        "appid","platformid"
+CREATE TABLE Game_Platform (
+    app_id int   NOT NULL,
+    platform_id int   NOT NULL,
+    CONSTRAINT pk_Game_Platform PRIMARY KEY (
+        app_id,platform_id
      )
 );
 
-CREATE TABLE "Game_Categorie" (
-    "appid" int   NOT NULL,
-    "categorieid" int   NOT NULL,
-    CONSTRAINT "pk_Game_Categorie" PRIMARY KEY (
-        "appid","categorieid"
+CREATE TABLE Game_Categorie (
+    app_id int   NOT NULL,
+    categorie_id int   NOT NULL,
+    CONSTRAINT pk_Game_Categorie PRIMARY KEY (
+        app_id,categorie_id
      )
 );
 
-CREATE TABLE "Game_Genre" (
-    "appid" int   NOT NULL,
-    "genreid" int   NOT NULL,
-    CONSTRAINT "pk_Game_Genre" PRIMARY KEY (
-        "appid","genreid"
+CREATE TABLE Game_Genre (
+    app_id int   NOT NULL,
+    genre_id int   NOT NULL,
+    CONSTRAINT pk_Game_Genre PRIMARY KEY (
+        app_id,genre_id
      )
 );
 
-CREATE TABLE "Game_Tag" (
-    "appid" int   NOT NULL,
-    "tagid" int   NOT NULL,
-    CONSTRAINT "pk_Game_Tag" PRIMARY KEY (
-        "appid","tagid"
+CREATE TABLE Game_Tag (
+    app_id int   NOT NULL,
+    tag_id int   NOT NULL,
+    CONSTRAINT pk_Game_Tag PRIMARY KEY (
+        app_id,tag_id
      )
 );
 
-ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_language" FOREIGN KEY("language")
-REFERENCES "Language" ("languageid");
+ALTER TABLE Games ADD CONSTRAINT fk_Games_language FOREIGN KEY(language)
+REFERENCES Language (language_id);
 
-ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_developer" FOREIGN KEY("developer")
-REFERENCES "Developers" ("developerid");
+ALTER TABLE Games ADD CONSTRAINT fk_Games_developer FOREIGN KEY(developer)
+REFERENCES Developers (developer_id);
 
-ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_publisher" FOREIGN KEY("publisher")
-REFERENCES "Publishers" ("publisherid");
+ALTER TABLE Games ADD CONSTRAINT fk_Games_publisher FOREIGN KEY(publisher)
+REFERENCES Publishers (publisher_id);
 
-ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_owners" FOREIGN KEY("owners")
-REFERENCES "Owners" ("ownersid");
+ALTER TABLE Games ADD CONSTRAINT fk_Games_owners FOREIGN KEY(owners)
+REFERENCES Owners (owners_id);
 
-ALTER TABLE "Game_Platform" ADD CONSTRAINT "fk_Game_Platform_appid" FOREIGN KEY("appid")
-REFERENCES "Games" ("appid");
+ALTER TABLE Game_Platform ADD CONSTRAINT fk_Game_Platform_app_id FOREIGN KEY(app_id)
+REFERENCES Games (app_id);
 
-ALTER TABLE "Game_Platform" ADD CONSTRAINT "fk_Game_Platform_platformid" FOREIGN KEY("platformid")
-REFERENCES "Platforms" ("platformid");
+ALTER TABLE Game_Platform ADD CONSTRAINT fk_Game_Platform_platform_id FOREIGN KEY(platform_id)
+REFERENCES Platforms (platform_id);
 
-ALTER TABLE "Game_Categorie" ADD CONSTRAINT "fk_Game_Categorie_appid" FOREIGN KEY("appid")
-REFERENCES "Games" ("appid");
+ALTER TABLE Game_Categorie ADD CONSTRAINT fk_Game_Categorie_app_id FOREIGN KEY(app_id)
+REFERENCES Games (app_id);
 
-ALTER TABLE "Game_Categorie" ADD CONSTRAINT "fk_Game_Categorie_categorieid" FOREIGN KEY("categorieid")
-REFERENCES "Categories" ("categorieid");
+ALTER TABLE Game_Categorie ADD CONSTRAINT fk_Game_Categorie_categorie_id FOREIGN KEY(categorie_id)
+REFERENCES Categories (category_id);
 
-ALTER TABLE "Game_Genre" ADD CONSTRAINT "fk_Game_Genre_appid" FOREIGN KEY("appid")
-REFERENCES "Games" ("appid");
+ALTER TABLE Game_Genre ADD CONSTRAINT fk_Game_Genre_app_id FOREIGN KEY(app_id)
+REFERENCES Games (app_id);
 
-ALTER TABLE "Game_Genre" ADD CONSTRAINT "fk_Game_Genre_genreid" FOREIGN KEY("genreid")
-REFERENCES "Genres" ("genreid");
+ALTER TABLE Game_Genre ADD CONSTRAINT fk_Game_Genre_genre_id FOREIGN KEY(genre_id)
+REFERENCES Genres (genre_id);
 
-ALTER TABLE "Game_Tag" ADD CONSTRAINT "fk_Game_Tag_appid" FOREIGN KEY("appid")
-REFERENCES "Games" ("appid");
+ALTER TABLE Game_Tag ADD CONSTRAINT fk_Game_Tag_app_id FOREIGN KEY(app_id)
+REFERENCES Games (app_id);
 
-ALTER TABLE "Game_Tag" ADD CONSTRAINT "fk_Game_Tag_tagid" FOREIGN KEY("tagid")
-REFERENCES "Tags" ("tagid");
+ALTER TABLE Game_Tag ADD CONSTRAINT fk_Game_Tag_tag_id FOREIGN KEY(tag_id)
+REFERENCES Tags (tag_id);
 
